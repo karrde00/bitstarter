10a717f8fb859e8a4558c635b807a979cc0f304a
Francois Mariette <karrde00@gmail.com>
2013-07-11 04:56:00 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,13 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send(index);
+  var buffer = new Buffer();
+  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
 });
-
-var index = fs.readFileSync('./index.html');
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
